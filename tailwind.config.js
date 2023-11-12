28dbb19a4c45af8dbe310067c805bae7e7237548
BartyDash <writetobartek@gmail.com>
2023-11-12 20:02:40 +0100

    add mobile styles for header & about section

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,14 @@ module.exports = {
     extend: {
       colors: {
         bg: '#020916',
-        primary: '#81FF7E'
+        primary: '#81FF7E',
+        lightGray: '#1F2937'
       },
       fontFamily: {
         'dm_sans': ['DM Sans', 'sans-serif']
+      },
+      maxWidth: {
+        'mywidth': '82rem'
       }
     },
   },
